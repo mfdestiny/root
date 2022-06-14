98785d034af1dce02041f75019a8f22a74fa7d44
mfdestiny <107088678+mfdestiny@users.noreply.github.com>
2022-06-14 11:08:34 -0500

    Add files via upload

---
+++ python/learn_python_the_hardway/ex5.py
@@ -0,0 +1,18 @@
+my_name = 'Zed A. Shaw'
+my_age = 32
+my_height = 60 #inches
+my_weight = 166 #pounds
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} inches tall.")
+print(f"He's {my_weight} pounds heavy.")
+print(f"Actually that's not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+#this line is tricky, try to get it ecactly right
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
