00d01c2a47dbd2234ec6af03bad6c2e938604206
mfdestiny <107088678+mfdestiny@users.noreply.github.com>
2022-06-14 13:43:22 -0500

    Add files via upload

---
+++ python/learn_python_the_hardway/ex10.py
@@ -0,0 +1,15 @@
+tabby = "\tI'm tabbed in."
+persian = "I'm split\non a line."
+black = "I'm \\ a \\ cat."
+
+fat = """
+I'll do a list:
+\t* Cat food
+\t* Fish
+\t* Catnip\n\t* Grass
+"""
+
+print(tabby)
+print(persian)
+print(black)
+print(fat)
