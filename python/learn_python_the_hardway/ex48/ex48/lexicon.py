94816288c87686392e7a46d9e1fc73aa6bfb862c
mfdestiny <107088678+mfdestiny@users.noreply.github.com>
2022-06-18 12:03:27 -0500

    refactored code and finished testing

--- python/learn_python_the_hardway/ex48/ex48/lexicon.py
+++ python/learn_python_the_hardway/ex48/ex48/lexicon.py
@@ -1,8 +1,6 @@
 class lexicon(object):
     def __init__(self):
-        self.sentence = [('direction', 'north'),('direction', 'south'),('direction', 'east'),('direction', 'west'),('direction', 'down'),('direction', 'up'),('direction', 'left'),('direction', 'right'),('direction', 'back'),('verbs', 'go'),('verbs', 'stop'),('verbs', 'kill'),('verbs', 'eat')]
-        #self.sentence = [('direction', 'north'),('direction', 'south'),('direction','east')]
-
+        self.sentence = [('direction', 'north'),('direction', 'south'),('direction', 'east'),('direction', 'west'),('direction', 'down'),('direction', 'up'),('direction', 'left'),('direction', 'right'),('direction', 'back'),('verb', 'go'),('verb', 'stop'),('verb', 'kill'),('verb', 'eat'),('stop', 'the'),('stop', 'in'),('stop', 'of'),('stop', 'from'),('stop', 'at'),('stop', 'it'),('noun', 'door'),('noun', 'bear'),('noun', 'princess'),('noun', 'cabinet'),('number', 1234),('number', 3),('number', 91234)]
 
 def convert_numbers(s):
     try:
@@ -10,16 +8,27 @@ def convert_numbers(s):
     except ValueError:
         return None
 
-
 def scan(request):
     lex = lexicon()
     requested = request.split(' ')
     results = []
 
-    for token1 in requested:
-        print(f"{token1}")
-        for idx, (word, token2) in enumerate(lex.sentence):
-            if (token1 == token2):
-                results.append(lex.sentence[idx])
+    for requested_word in requested:
+        found_word = False
+        found_number = False
+
+        for item in range(0,len(lex.sentence)):#iterates through requests
+            if (not found_word): #keep searching for word
+                if (requested_word == lex.sentence[item][1]):#word matches add to results
+                    found_word = True
+                    results.append(lex.sentence[item]) #add to list
+
+            if (not found_number):#search for number
+                if (convert_numbers(requested_word) == lex.sentence[item][1]):#number matches
+                    found_number = True
+                    results.append(lex.sentence[item]) #add to list
+
+        if (not found_number and not found_word): # if nothing was matches return error
+            results.append(('error', requested_word))
 
     return results
