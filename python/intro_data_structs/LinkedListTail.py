bf01ff38021b386f971e9298056133ea315dd0d3
mfdestiny <107088678+mfdestiny@users.noreply.github.com>
2022-06-27 11:02:55 -0400

    added length functionality

--- python/intro_data_structs/LinkedListTail.py
+++ python/intro_data_structs/LinkedListTail.py
@@ -7,21 +7,26 @@ class LinkedListTail:
     def __init__(self):
         self._head = None
         self._tail = None
+        self._length = 0
 
     def addfirst(self,item):
         if self._head is None:
             self._head = ListNode(item,self._head)
             if self._tail is None:
                 self._tail = self._head
+        self._length += 1
 
     def addlast(self,item):
         if self._head is None:
             self._head = addfirst(item)
         else:
+        self._lenght += 1
         self._tail.link = ListNode(item) #update old tail link with new node
         self._tail = self._tail.link #set tail to point to node
 
+
     def removefirst(self):
+        self._length -= 1
         item = self._head.data
         self._head = self._head.link
         if self._head is None: #if list only had one to begin with, now the list is empty
@@ -32,6 +37,7 @@ class LinkedListTail:
         if self._head is self._tail:
             return self.removefirst()
         else:
+            self._length -= 1
             item = self._tail.data
             node = self._head
             while node.link is not self._tail: #find node before tail.
@@ -39,3 +45,6 @@ class LinkedListTail:
             self._tail = node #set new tail to node before tail
             self._tail.link = None #update new tail status
             return item
+
+    def __len__(self):
+        return self._length
